6987e9fccd99bd966d61556d3bb1b0477f88219f
Umbrien <serhii.kaluhin@nure.ua>
2021-02-21 22:46:07 +0200

    Made insert method unstatic

--- src/com/company/lab_1/main.java
+++ src/com/company/lab_1/main.java
@@ -11,16 +11,14 @@ class SortedLinkedList {
         Node(int d) { data = d; }
     }
 
-    // TODO remove static
-    public static SortedLinkedList insert(SortedLinkedList list, int data) {
+    public void insert(int data) {
         Node new_node = new Node(data);
 
-        // if list is empty
-        if (list.head == null || list.head.data >= new_node.data) {
-            new_node.next = list.head;
-            list.head = new_node;
+        if (head == null || head.data >= new_node.data) {
+            new_node.next = head;
+            head = new_node;
         } else {
-            Node current = list.head;
+            Node current = head;
 
             while (current.next != null && current.next.data < new_node.data) {
                 current = current.next;
@@ -28,7 +26,6 @@ class SortedLinkedList {
             new_node.next = current.next;
             current.next = new_node;
         }
-        return  list;
     }
 
     public static void printList(SortedLinkedList list) {
@@ -49,15 +46,15 @@ class Main {
     public static void main(String[] args) {
 
         SortedLinkedList sortedLinkedList = new SortedLinkedList();
-        SortedLinkedList.insert(sortedLinkedList, 4);
-        SortedLinkedList.insert(sortedLinkedList, 3);
-        SortedLinkedList.insert(sortedLinkedList, 2);
-        SortedLinkedList.insert(sortedLinkedList, 6);
-        SortedLinkedList.insert(sortedLinkedList, 5);
-        SortedLinkedList.insert(sortedLinkedList, 5);
-        SortedLinkedList.insert(sortedLinkedList, 6);
-        SortedLinkedList.insert(sortedLinkedList, 1);
-        SortedLinkedList.insert(sortedLinkedList, 2);
+        sortedLinkedList.insert(4);
+        sortedLinkedList.insert(3);
+        sortedLinkedList.insert(2);
+        sortedLinkedList.insert(6);
+        sortedLinkedList.insert(5);
+        sortedLinkedList.insert(5);
+        sortedLinkedList.insert(6);
+        sortedLinkedList.insert(1);
+        sortedLinkedList.insert(2);
         SortedLinkedList.printList(sortedLinkedList);
     }
 }
