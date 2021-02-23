3c11e3090992078b463dcf7a4b9dfbb70a081b29
Umbrien <serhii.kaluhin@nure.ua>
2021-02-23 19:10:51 +0200

    List works with any <T extends Comparable>

--- src/com/company/lab_1/main.java
+++ src/com/company/lab_1/main.java
@@ -1,26 +1,26 @@
 package com.company.lab_1;
 
 
-class SortedLinkedList {
+class SortedLinkedList<T extends Comparable<T>> {
     Node head;
 
     class Node {
-        int data;
+        T data;
         Node next;
 
-        Node(int d) { data = d; }
+        Node(T d) { data = d; }
     }
 
-    public void insert(int data) {
+    public void insert(T data) {
         Node new_node = new Node(data);
 
-        if (head == null || head.data >= new_node.data) {
+        if (head == null || head.data.compareTo(new_node.data) >= 0) {
             new_node.next = head;
             head = new_node;
         } else {
             Node current = head;
 
-            while (current.next != null && current.next.data < new_node.data) {
+            while (current.next != null && current.next.data.compareTo(new_node.data) < 0) {
                 current = current.next;
             }
             new_node.next = current.next;
@@ -28,7 +28,7 @@ class SortedLinkedList {
         }
     }
 
-    public void mergeList(SortedLinkedList list) {
+    public void mergeList(SortedLinkedList<T> list) {
         Node current = list.head;
 
         while (current != null) {
@@ -37,10 +37,11 @@ class SortedLinkedList {
         }
     }
 
-    public static SortedLinkedList fromArray(int[] arr) {
-        SortedLinkedList list = new SortedLinkedList();
 
-        for (int i : arr) {
+    public static <S extends Comparable<S>> SortedLinkedList<S> fromArray(S[] arr) {
+        SortedLinkedList<S> list = new SortedLinkedList<>();
+
+        for (S i : arr) {
             list.insert(i);
         }
 
@@ -48,8 +49,8 @@ class SortedLinkedList {
     }
 
 
-    public static void printList(SortedLinkedList list) {
-        Node currentNode = list.head;
+    public void printList() {
+        Node currentNode = this.head;
 
         System.out.print("SortedLinkedList: ");
 
@@ -65,12 +66,14 @@ class Main {
 
     public static void main(String[] args) {
 
-        SortedLinkedList list = SortedLinkedList.fromArray(new int[] {1, 6, 3, 1, 2, 5});
-        SortedLinkedList.printList(list);
+        SortedLinkedList<Integer> list = SortedLinkedList.fromArray(new Integer[] {1, 6, 3, 1, 2, 5});
+        list.printList();
+        SortedLinkedList<String> stringList = SortedLinkedList.fromArray(new String[] {"a", "b", "bb", "aa", "ab"});
+        stringList.printList();
 
-        SortedLinkedList l1 = SortedLinkedList.fromArray(new int[] {1, 2});
-        SortedLinkedList l2 = SortedLinkedList.fromArray(new int[] {3, 4});
+        SortedLinkedList<Integer> l1 = SortedLinkedList.fromArray(new Integer[] {1, 2});
+        SortedLinkedList<Integer> l2 = SortedLinkedList.fromArray(new Integer[] {3, 4});
         l1.mergeList(l2);
-        SortedLinkedList.printList(l1);
+        l1.printList();
     }
 }
