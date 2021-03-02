31faa3607400a117a9d20ab2d6244e9817f687c3
Umbrien <serhii.kaluhin@nure.ua>
2021-03-02 20:54:44 +0200

    Implemented isEmpty(), search(), partially implemented addItem()

--- src/com/company/lab_2/BalancedBinarySearchTree.java
+++ src/com/company/lab_2/BalancedBinarySearchTree.java
@@ -6,21 +6,56 @@ public class BalancedBinarySearchTree<T extends Comparable<T>> {
 
     class Node {
         T data;
-        BalancedBinarySearchTree.Node left;
-        BalancedBinarySearchTree.Node right;
+        Node left, right;
 
         Node(T d) { data = d; }
     }
 
-    boolean isEmpty() {return true;}
+    boolean isEmpty() {return root == null;}
 
     boolean isFull() {return !isEmpty();}
 
     int size() {return -1;}
 
-    void addItem(T item) {}
+    private Node recursiveAdd(Node current, T item) {
+        if (current == null) {
+            return new Node(item);
+        }
 
-    boolean search(T item) {return false;}
+        if(item.compareTo(current.data) < 0) {
+            current.left = recursiveAdd(current.left, item);
+        } else if (item.compareTo(current.data) > 0) {
+            current.right = recursiveAdd(current.right, item);
+        } else {
+            //value already exists
+            return current;
+        }
+
+        return current;
+    }
+
+    void addItem(T item) {
+        // Normal insert
+        root = recursiveAdd(root, item);
+
+        // TODO check if balanced. If not, go balance, emo kid
+    }
+
+    private boolean recursiveSearch(Node current, T item) {
+        if(current == null) {
+            return false;
+        }
+        if (item == current.data) {
+            return true;
+        }
+        return item.compareTo(current.data) < 0
+                ? recursiveSearch(current.left, item)
+                : recursiveSearch(current.right, item);
+    }
+
+    boolean search(T item) {
+        return recursiveSearch(root, item);
+    }
 
     int depthOf(T item) {return -1;}
 
